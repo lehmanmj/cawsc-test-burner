67a27cf139d1a6da16b36e571a702a4e9a59dfc8
Michael Lehmann <lehmanmj@amazon.com>
2025-08-29 11:42:18 -0700

    write to file

--- fetch-token.js
+++ fetch-token.js
@@ -1,8 +1,10 @@
 import core from "@actions/core";
+import fs from "fs";
 
 async function getIDTokenAction() {
    const id_token1 = await core.getIDToken();
    return id_token1;
 }
 let whatever = await getIDTokenAction();
-console.log(whatever);
+
+fs.writeFile("token.txt", whatever);
