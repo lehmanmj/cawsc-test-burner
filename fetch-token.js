bec79a5ca0d035e363b85d01616cee5193cb6cde
Michael Lehmann <lehmanmj@amazon.com>
2025-08-29 11:38:01 -0700

    try import instead?

--- fetch-token.js
+++ fetch-token.js
@@ -1,4 +1,4 @@
-const core = require('@actions/core');
+const core = import('@actions/core');
 async function getIDTokenAction() {
    const id_token1 = await core.getIDToken();
    return id_token1;
